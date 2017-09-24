71d698fa6344a5173acf179bc433ccd0baf9ec5a
Andy Osborne <cao89@email.arizona.edu>
2017-09-24 15:55:46 -0700

    Fix deploy.php file name

--- deploy.php
+++ deploy.php
@@ -1,25 +1,33 @@
 <?php
 namespace Deployer;
+
 require 'recipe/common.php';
+
 // Project name
 set('application', 'my_project');
+
 // Project repository
 set('repository', 'https://github.com/caosborne89/Wp-DrinkingWithOwen.git');
+
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true); 
+
 // Shared files/dirs between deploys 
-set('shared_files', []);
+set('shared_files', ['.env']);
 set('shared_dirs', []);
+
 // Writable dirs by web server 
 set('writable_dirs', []);
-// Keep up to three releases
-set('keep_releases', 5);
+
+
 // Hosts
+
 host('138.197.42.218')
-    ->stage('production') 
     ->set('deploy_path', '/var/www/html');    
     
+
 // Tasks
+
 desc('Deploy your project');
 task('deploy', [
     'deploy:info',
@@ -36,5 +44,6 @@ task('deploy', [
     'cleanup',
     'success'
 ]);
+
 // [Optional] If deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
